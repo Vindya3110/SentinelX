f2ff9eb6808b2194b47185875724c75415de0587
Vindya3110 <107698508+Vindya3110@users.noreply.github.com>
2025-10-22 13:53:20 +0530

    Add files via upload

---
+++ SentinelX/src/main/java/com/sentries/SentinelX/SentinelXApplication.java
@@ -0,0 +1,13 @@
+package com.sentries.SentinelX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelXApplication.class, args);
+	}
+
+}
---
+++ SentinelX/src/test/java/com/sentries/SentinelX/SentinelXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentries.SentinelX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
