9c67858997bddce63f83caaf9dc8dc039511d9d6
Shaamam <shaama0704@gmail.com>
2025-12-15 12:00:33 +0530

    Refactor project structure and update configuration files

---
+++ apps/githubmcp/src/main/java/io/vindhya/githubmcp/GithubmcpApplication.java
@@ -0,0 +1,13 @@
+package io.vindhya.githubmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubmcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubmcpApplication.class, args);
+	}
+
+}
---
+++ apps/githubmcp/src/test/java/io/vindhya/githubmcp/GithubmcpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vindhya.githubmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubmcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
